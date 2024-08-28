e3276a81fbd8c3dfc21f562bc6d2f8c71928b441
Franco <franco1985@gmail.com>
2024-08-27 23:21:02 -0300

    gracias gpt

--- src/crud/pedido.py
+++ src/crud/pedido.py
@@ -40,41 +40,6 @@ def get_pedido_full_info(db: sessionmaker, id_pedido: int):
     return pedido_completo
 
 
-# def get_insumos_pedido(db: sessionmaker, id_pedido: int):
-#     res = get_pedido_full_info(db=db, id_pedido=id_pedido)
-
-#     acumulado = defaultdict(lambda: {'cantidad_total': 0, 'precio_total': 0.0})
-#     for item in res['detalle_pedido']:
-#         producto_id = item.id_producto
-#         acumulado[producto_id]['nombre_producto'] = item.nombre_producto
-#         acumulado[producto_id]['cantidad_total'] += item.cantidad_producto
-#         acumulado[producto_id]['precio_total'] += float(item.total)
-
-#     resultado = []
-
-#     for producto_id, valores in acumulado.items():
-#         insumos = get_insumos_productos(db=db, id_producto=producto_id)
-
-#         detalle_insumo = [
-#             {
-#                 'insumo_id': item_insumo.id_insumo,
-#                 'nombre_insumo': item_insumo.nombre_insumo,
-#                 'total_insumo': item_insumo.cantidad * valores['cantidad_total'],
-#                 'unidad_medida': item_insumo.nombre_unidad_medida
-#             }
-#             for item_insumo in insumos
-#         ]
-
-#         resultado.append({
-#             'producto': producto_id,
-#             'nombre_producto': valores['nombre_producto'],
-#             'cantidad_total': valores['cantidad_total'],
-#             'precio_total': valores['precio_total'],
-#             'detalle_insumo': detalle_insumo
-#         })
-
-#     return resultado
-
 def get_insumos_pedido(db: sessionmaker, id_pedido: int):
     res = get_pedido_full_info(db=db, id_pedido=id_pedido)
 
